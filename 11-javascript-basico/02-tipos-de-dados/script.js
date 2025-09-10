e2210519b1380e8b88ebd2d8160a8690049e90c3
Jonathan Freitas <jonathanbfreitas7@gmail.com>
2025-09-10 17:52:33 -0300

    tipos-de-dados

---
+++ 11-javascript-basico/02-tipos-de-dados/script.js
@@ -0,0 +1,24 @@
+const text = "JavaScript";
+const frase = 'Isso é o meu "jogo"';
+
+console.log(text + " " + frase);
+
+let total = 0;
+
+const compras = 30;
+const imposto = 10;
+
+//Da pra utilizar sinal de * / -
+
+total = compras + imposto;
+
+console.log(total);
+
+const strings = "20" + "20";
+const numbers = 20 + 20;
+
+console.log(numbers);
+
+const transformar = Number("20") + 20;
+
+console.log(transformar);
