666a9c081d7dafef86d3b15a922e5e0defc93d2e
Jonathan Freitas <jonathanbfreitas7@gmail.com>
2025-09-19 18:13:38 -0300

    eventos

---
+++ 11-javascript-basico/06-eventos/script.js
@@ -0,0 +1,13 @@
+//objeto
+const curso = {
+  //propriedade
+  nome: "JavaScript",
+  //funcao declarada sem precisar escreve function
+  completar() {
+    //metodo
+    console.log("Curso Completo");
+  },
+};
+
+console.log(curso.nome);
+curso.completar();
