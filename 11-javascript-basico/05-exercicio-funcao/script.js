33d250115c52c7d5f3a186e46d239d4f96628d7d
Jonathan Freitas <jonathanbfreitas7@gmail.com>
2025-09-19 15:41:49 -0300

    resolucao-exercicio-funcao

--- 11-javascript-basico/05-exercicio-funcao/script.js
+++ 11-javascript-basico/05-exercicio-funcao/script.js
@@ -1,15 +1,44 @@
+//primeira forma de realizar o exercicio:
+
+// function livro(nome, ano, autor) {
+//   const nomeMaiusculo = nome.toUpperCase();
+//   const anoLivro = 2050 - ano;
+//   const frase = nome + " por " + autor;
+
+//   const retorno = {
+//     nome: nomeMaiusculo,
+//     anoLivro,
+//     frase,
+//   };
+
+//   return retorno;
+// }
+
+// const livro1 = livro("O Senhor dos Anéis", 1954, "J. R. R. Tolkien");
+// console.log(livro1.frase);
+
+//segunda forma de realizar o exercicio:
+
+// function livro(nome, ano, autor) {
+//   const retorno = {
+//     nome: nome.toUpperCase(),
+//     anoLivro: 2050 - ano,
+//     frase: nome + " por " + autor,
+//   };
+//   return retorno;
+// }
+
+// const livro1 = livro("O Senhor dos Anéis", 1954, "J. R. R. Tolkien");
+// console.log(livro1.frase);
+
+//terceira forma de realizar o exercicio:
+
 function livro(nome, ano, autor) {
-  const nomeMaiusculo = nome.toUpperCase();
-  const anoLivro = 2050 - ano;
-  const frase = nome + " por " + autor;
-
-  const retorno = {
-    nomeMaiusculo,
-    anoLivro,
-    frase,
+  return {
+    nome: nome.toUpperCase(),
+    anoLivro: 2050 - ano,
+    frase: nome + " por " + autor,
   };
-
-  return retorno;
 }
 
 const livro1 = livro("O Senhor dos Anéis", 1954, "J. R. R. Tolkien");
