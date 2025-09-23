7b22cf09fd2a9c0fef865b4d46df487477adb04c
Jonathan Freitas <jonathanbfreitas7@gmail.com>
2025-09-23 00:06:21 -0300

    eventos-event

--- 11-javascript-basico/06-eventos/script.js
+++ 11-javascript-basico/06-eventos/script.js
@@ -1,13 +1,45 @@
-//objeto
-const curso = {
-  //propriedade
-  nome: "JavaScript",
-  //funcao declarada sem precisar escreve function
-  completar() {
-    //metodo
-    console.log("Curso Completo");
-  },
-};
+//METODO
+// //objeto
+// const curso = {
+//   //propriedade
+//   nome: "JavaScript",
+//   //funcao declarada sem precisar escrever function
+//   completar() {
+//     //metodo
+//     console.log("Curso Completo");
+//   },
+// };
 
-console.log(curso.nome);
-curso.completar();
+// console.log(curso.nome);
+// curso.completar();
+
+// const curso = {
+//   nome: "JavaScript",
+//   completar() {
+//     console.log(this.nome);
+//   },
+// };
+
+// curso.completar();
+
+//EVENTO
+// const ativar = document.querySelector(".ativar");
+
+// function ativarAoClick() {
+//   console.log("Clicou em:", ativar);
+// }
+
+// ativar.addEventListener("click", ativarAoClick);
+
+// console.log(ativar);
+
+const ativar = document.querySelector(".ativar");
+
+/** @param {MouseEvent} event  */
+function ativarAoClick(event) {
+  console.log(event.x);
+}
+
+ativar.addEventListener("click", ativarAoClick);
+
+console.log(ativar);
