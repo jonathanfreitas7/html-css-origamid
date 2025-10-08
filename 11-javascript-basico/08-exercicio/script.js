cd81755e04b05758e6ca7dc1638936f3fc3274bf
Jonathan Freitas <jonathanbfreitas7@gmail.com>
2025-10-08 17:17:34 -0300

    exercicio

--- 11-javascript-basico/08-exercicio/script.js
+++ 11-javascript-basico/08-exercicio/script.js
@@ -1,8 +1,8 @@
 const circulo = document.querySelector("#circulo");
 
 function seguirMouse(event) {
-  circulo.style.top = y + "px";
-  circulo.style.left = x + "px";
+  circulo.style.top = event.y + "px";
+  circulo.style.left = event.x + "px";
 }
 
 window.addEventListener("mousemove", seguirMouse);
