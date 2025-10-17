a527d9ff330c88dfd76de0c3ebb04d7e684840ed
Jonathan Freitas <jonathanbfreitas7@gmail.com>
2025-10-17 10:25:36 -0300

    loop

---
+++ 11-javascript-basico/12-arrays/script.js
@@ -0,0 +1,9 @@
+//loop
+
+const lista = ["JavaScript", "HTML", "CSS"];
+const body = document.querySelector("body");
+
+for (let index = 0; index < lista.length; index++) {
+  const item = lista[index];
+  body.innerHTML += "<li>" + lista[index] + "</li>";
+}
