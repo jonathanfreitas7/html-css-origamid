64d70319fcfa08576b762651342493faa58e4178
Jonathan Freitas <jonathanbfreitas7@gmail.com>
2025-10-21 22:29:40 -0300

    exercicioarray

---
+++ 11-javascript-basico/15-metodospropriedades/script.js
@@ -0,0 +1,15 @@
+const lista = ["JavaScript", "HTML", "CSS"];
+
+// retorna o total
+const total = lista.length;
+
+// remove o último
+const ultimo = lista.pop();
+
+// remove o primeiros
+const primeiro = lista.shift();
+
+// adiciona ao final
+lista.push("PHP");
+
+console.log(lista);
